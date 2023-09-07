2cfa06214f8d4bc180154b98dff8c7239e1a18f5
ajoherron <ajoherron@gmail.com>
2023-09-07 16:53:27 -0400

    adding example file to show how Python files should be formatted

---
+++ example.py
@@ -0,0 +1,43 @@
+'''
+This file is an example of how Python files should be laid out.
+Start with a descriptive docstring at the top.
+'''
+
+# Standard library imports first
+import os
+import sys
+
+# 3rd-party library imports second
+import numpy as np
+import pandas as pd
+
+# Local imports third
+from steps import step0
+
+# Global constants and variables (all caps)
+FOO = 13
+BAR = 1738
+
+# Classes
+class Hello():
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        return f'Hello, {self.name}!'
+    
+# Functions
+def square(x):
+    return x**2
+
+# Script logic
+if __name__ == '__main__':
+
+    # Print greeting from class
+    obj = Hello('Alex')
+    greeting = obj.greet()
+    print(greeting)
+
+    # Calculate square using function
+    result = square(FOO)
+    print(f'{FOO} squared is {result}')
